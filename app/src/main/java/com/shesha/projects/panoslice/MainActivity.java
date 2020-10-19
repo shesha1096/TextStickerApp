d24e1f4bb55d51656e7a8f56d6bf0c8ec7e485f8
Shesha Shankar <shesha1096@gmail.com>
2020-10-19 21:44:20 +1100

    Adding functionalities to each text sticker being added to the view.

--- app/src/main/java/com/shesha/projects/panoslice/MainActivity.java
+++ app/src/main/java/com/shesha/projects/panoslice/MainActivity.java
@@ -1,32 +1,46 @@
 package com.shesha.projects.panoslice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.xiaopo.flying.sticker.BitmapStickerIcon;
+import com.xiaopo.flying.sticker.DeleteIconEvent;
+import com.xiaopo.flying.sticker.FlipHorizontallyEvent;
+import com.xiaopo.flying.sticker.FlipVerticallyEvent;
+import com.xiaopo.flying.sticker.Sticker;
 import com.xiaopo.flying.sticker.StickerView;
 import com.xiaopo.flying.sticker.TextSticker;
+import com.xiaopo.flying.sticker.ZoomIconEvent;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import top.defaults.drawabletoolbox.DrawableBuilder;
+
 public class MainActivity extends AppCompatActivity implements InputDialog.InputDialogListener {
+    private static final String TAG = "PANOSLICE_APP";
     private StickerView stickerView;
     private List<TextSticker> textStickers;
     private Button addStickerButton;
@@ -43,6 +57,82 @@ public class MainActivity extends AppCompatActivity implements InputDialog.Input
         stickerView = (StickerView) findViewById(R.id.stickerView);
         addStickerButton = (Button) findViewById(R.id.text_sticker_btn);
         editCanvasButton = (Button) findViewById(R.id.editBtn);
+        BitmapStickerIcon deleteIcon = new BitmapStickerIcon(ContextCompat.getDrawable(this,
+                com.xiaopo.flying.sticker.R.drawable.sticker_ic_close_white_18dp),
+                BitmapStickerIcon.LEFT_TOP);
+        deleteIcon.setIconEvent(new DeleteIconEvent());
+
+        BitmapStickerIcon zoomIcon = new BitmapStickerIcon(ContextCompat.getDrawable(this,
+                com.xiaopo.flying.sticker.R.drawable.sticker_ic_scale_white_18dp),
+                BitmapStickerIcon.RIGHT_BOTOM);
+        zoomIcon.setIconEvent(new ZoomIconEvent());
+
+        BitmapStickerIcon flipIcon = new BitmapStickerIcon(ContextCompat.getDrawable(this,
+                com.xiaopo.flying.sticker.R.drawable.sticker_ic_flip_white_18dp),
+                BitmapStickerIcon.RIGHT_TOP);
+        flipIcon.setIconEvent(new FlipVerticallyEvent());
+
+        BitmapStickerIcon scaleIcon =
+                new BitmapStickerIcon(ContextCompat.getDrawable(this, R.drawable.ic_baseline_pause_24),
+                        BitmapStickerIcon.LEFT_BOTTOM);
+        scaleIcon.setIconEvent(new ScaleIconEvent());
+
+
+         /*
+        BitmapStickerIcon rotateIcon =
+                new BitmapStickerIcon(ContextCompat.getDrawable(this, R.drawable.ic_baseline_rotate_left_24),
+                        BitmapStickerIcon.LEFT_BOTTOM);
+        rotateIcon.setIconEvent(new ScaleIconEvent());
+
+          */
+
+        stickerView.setIcons(Arrays.asList(deleteIcon, zoomIcon, flipIcon, scaleIcon ));
+        stickerView.setBackgroundColor(Color.WHITE);
+        stickerView.setLocked(false);
+        stickerView.setConstrained(true);
+
+        stickerView.setOnStickerOperationListener(new StickerView.OnStickerOperationListener() {
+            @Override
+            public void onStickerAdded(@NonNull Sticker sticker) {
+                Log.d(TAG, "onStickerAdded");
+            }
+
+            @Override
+            public void onStickerClicked(@NonNull Sticker sticker) {
+                //stickerView.removeAllSticker();
+                if (sticker instanceof TextSticker) {
+                    ((TextSticker) sticker).setTextColor(Color.RED);
+                    stickerView.replace(sticker);
+                    stickerView.invalidate();
+                }
+                Log.d(TAG, "onStickerClicked");
+            }
+
+            @Override
+            public void onStickerDeleted(@NonNull Sticker sticker) {
+                Log.d(TAG, "onStickerDeleted");
+            }
+
+            @Override
+            public void onStickerDragFinished(@NonNull Sticker sticker) {
+                Log.d(TAG, "onStickerDragFinished");
+            }
+
+            @Override
+            public void onStickerZoomFinished(@NonNull Sticker sticker) {
+                Log.d(TAG, "onStickerZoomFinished");
+            }
+
+            @Override
+            public void onStickerFlipped(@NonNull Sticker sticker) {
+                Log.d(TAG, "onStickerFlipped");
+            }
+
+            @Override
+            public void onStickerDoubleTapped(@NonNull Sticker sticker) {
+                Log.d(TAG, "onDoubleTapped: double tap will be with two click");
+            }
+        });
         if (getIntent().getExtras() != null)
         {
             if ((HashMap<Integer, HashMap<String, String>>) getIntent().getExtras().get("Sticker_Info") != null)
@@ -136,6 +226,7 @@ public class MainActivity extends AppCompatActivity implements InputDialog.Input
             textSticker.setTextAlign(Layout.Alignment.ALIGN_CENTER);
             textSticker.resizeText();
             textSticker.setFlippedHorizontally(true);
+            textSticker.setDrawable(ContextCompat.getDrawable(getApplicationContext(), com.xiaopo.flying.sticker.R.drawable.sticker_transparent_background));
             textStickers.add(textSticker);
             stickerView.addSticker(textSticker);
         }
